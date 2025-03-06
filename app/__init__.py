4b5cb1627622cf20514c7a2b4aadafb473e1f10c
Timur <mrt200228@gmail.com>
2025-03-07 02:39:56 +0300

    celery fixed

--- app/__init__.py
+++ app/__init__.py
@@ -25,6 +25,13 @@ def make_celery(app):
     )
 
     celery.conf.update(app.config)
+    
+    class ContextTask(celery.Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return self.run(*args, **kwargs)
+    
+    celery.Task = ContextTask
     return celery
 
 def create_app(test_mode=False):
